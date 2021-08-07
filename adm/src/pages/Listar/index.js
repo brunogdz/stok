4c01cda6e9492f6cba91362204227ac2084773ae
Bruno Gomes <brunog.vix@gmail.com>
2021-08-06 22:42:24 -0300

    index mexido

--- adm/src/pages/Listar/index.js
+++ adm/src/pages/Listar/index.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Listar = () => {
-    return(
+    const [data, setData] = useState([])
+    const listarProdutos = async => {
+        var valores = [
+            {
+                "id": 2,
+                "nome": "Camiseta Canal",
+                "valor": 69.99,
+                "quantidade": 25
+            },
+            {
+                "id": 1,
+                "nome": "Mouse Gamer",
+                "valor": 80.00,
+                "quantidade": 2
+            },
+            {
+                "id": 3,
+                "nome": "Caneca Canal",
+                "valor": 24.00,
+                "quantidade": 5
+            }
+        ]
+        setData(valores);
+    }
+    useEffect(() => {
+        listarProdutos();
+    }, [])
+
+    return (
         <>
             <h1>Listar</h1>
+
+            <table>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Nome</td>
+                        <td>Valor</td>
+                        <td>Quantidade</td>
+                        <td>Ações</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(produto => (
+                        <tr>
+                            <td>{produto.id}</td>
+                            <td>{produto.nome}</td>
+                            <td>{produto.valor}</td>
+                            <td>{produto.quantidade}</td>
+                            <td>Visualizar Editar Apagar</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     );
 }
